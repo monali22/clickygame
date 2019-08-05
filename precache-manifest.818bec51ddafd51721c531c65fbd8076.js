df8a1c1c23d343abc9a596e023888d5a1d38b0cf
Monali Chandurkar <chandurkar.monali123@gmail.com>
2019-08-04 22:33:17 -0700

    Updates

---
+++ precache-manifest.818bec51ddafd51721c531c65fbd8076.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0c5b4b95a78e2985b4d307eb66ae560",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "bf144cb25c96d66d268e",
+    "url": "/clickygame/static/css/main.47bd904f.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/clickygame/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "bf144cb25c96d66d268e",
+    "url": "/clickygame/static/js/main.a4fe2f2e.chunk.js"
+  },
+  {
+    "revision": "0f0146524616eea72d1b",
+    "url": "/clickygame/static/js/runtime~main.bb8261cb.js"
+  }
+]);
